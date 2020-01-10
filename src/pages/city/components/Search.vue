eaf2021bd885d0e86d4c0c81f90517f2fd4dea8a
song285 <alen285@qq.com>
2020-01-10 18:08:04 +0800

    city search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="search">
+		<input type="text" placeholder="输入城市名拼音" class="search-input" />
+	</div>
+</template>
+
+<script>
+export default {
+  name: "CitySearch"
+}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.search
+		height: .72rem
+		background: $bgColor
+		padding: 0 .1rem
+		.search-input
+			box-sizing: border-box
+			height:.62rem
+			width:100%
+			text-align:center
+			border-radius:.06rem
+			color:#666
+			padding: 0 .2rem
+</style>
